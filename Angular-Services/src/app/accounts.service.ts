3cf7a81567120101a0be3e8d9b77f2e4db6297bf
Ajay Singh <aksingh1493@gmail.com>
2018-02-12 23:15:18 +0530

    Services

---
+++ Angular-Services/src/app/accounts.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AccountsService {
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'Active'
+        },
+        {
+            name: 'Test Account',
+            status: 'Inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'unknown'
+        }
+    ];
+
+    addAccount(name: string, status: string) {
+        this.accounts.push({name: name, status: status});
+    }
+    updateStatus(id: number, status: string) {
+        this.accounts[id].status = status;
+    }
+}
---
+++ Angular-Services/src/app/new-account/new-account.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from 'app/logging.service';
+import { AccountsService } from 'app/accounts.service';
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+  //@Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService, 
+              private accountsService: AccountsService){}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+   this.accountsService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+  }
+}
